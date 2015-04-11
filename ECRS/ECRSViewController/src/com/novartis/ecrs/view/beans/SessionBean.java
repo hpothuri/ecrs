3f0cc7f4b0a3d5267cdaaa1231d8371bd250a91e
npotti <nitish_mkumar@yahoo.com>
2015-04-11 15:54:32 +0530

    Template and login page created.

---
+++ ECRS/ECRSViewController/src/com/novartis/ecrs/view/beans/SessionBean.java
@@ -0,0 +1,33 @@
+package com.novartis.ecrs.view.beans;
+
+import oracle.adf.share.ADFContext;
+import oracle.adf.share.security.SecurityContext;
+
+public class SessionBean {
+    public SessionBean() {
+        super();
+    }
+    
+    private String userRole;
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public String getUserRole() {
+        if(userRole == null){
+            SecurityContext secCtx =
+                ADFContext.getCurrent().getSecurityContext();
+            String[] roles = secCtx.getUserRoles();
+            if(roles != null){
+                for(String role : roles){
+                    if("BSL".equalsIgnoreCase(role) || "MQM".equalsIgnoreCase(role) || "TASL".equalsIgnoreCase(role) || "ML".equalsIgnoreCase(role)){
+                        userRole = role;
+                        break;
+                    }
+                }
+            }
+        }
+        return userRole;
+    }
+}
