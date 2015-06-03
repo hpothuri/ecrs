97cf4ea4aa2a9f0e108f3975c068ca4ba0d63d09
hpothuri <harish.pvr@hotmail.com>
2015-06-03 11:37:20 +0530

    Session timeout - redirecting to home page whn current page is a protected one

--- ECRS/ECRSViewController/src/com/novartis/ecrs/ui/bean/application/TimeOutListener.java
+++ ECRS/ECRSViewController/src/com/novartis/ecrs/ui/bean/application/TimeOutListener.java
@@ -1,5 +1,7 @@
 package com.novartis.ecrs.ui.bean.application;
 
+import com.novartis.ecrs.ui.bean.ManageCRSBean;
+
 import java.io.IOException;
 
 import java.io.PrintWriter;
@@ -22,20 +24,24 @@ import javax.servlet.http.HttpServletResponse;
 
 import oracle.adf.share.logging.ADFLogger;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
 import weblogic.servlet.security.ServletAuthentication;
 
 public class TimeOutListener implements Filter {
 
     private FilterConfig filterConfig = null;
     private String initParam = null;
-
+    private static final Logger logger = Logger.getLogger(TimeOutListener.class);
+    private String timeoutPage = "faces/Home";
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void init(final FilterConfig filterConfig) throws ServletException {
-//        logger.info("Init SessionExpiredFilter.");
+        logger.info("Init TimeOutListener");
         this.filterConfig = filterConfig;
     }
 
@@ -47,60 +53,64 @@ public class TimeOutListener implements Filter {
         filterConfig = null;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void doFilter(final ServletRequest request,
-                         final ServletResponse response,
-                         final FilterChain chain) throws IOException,
-                                                         ServletException {
-//        logger.fine("Check if the session is valid.");
-        final HttpServletRequest httpServletRequest =
-            (HttpServletRequest)request;
-        final String adfCtrlState = request.getParameter("_adf.ctrl-state");
-        final String requestedSession =
-            httpServletRequest.getRequestedSessionId();
-        final String currentWebSession =
-            httpServletRequest.getSession().getId();
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException,
+                                                                                                           ServletException {
+        logger.info("inside doFilter");
+        if ((request instanceof HttpServletRequest) && (response instanceof HttpServletResponse)) {
+            HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+            HttpServletResponse httpServletResponse =
+                (HttpServletResponse)response; 
 
-//        logger.fine("Filter info: \n" +
-//                " adfCtrolState: " + adfCtrlState + "\n" +
-//                " requestedSession: " + requestedSession + "\n" +
-//                " currentWebSession " + currentWebSession);
+            if (isSessionControlRequiredForThisResource(httpServletRequest)) { 
+                if (isSessionInvalid(httpServletRequest)) {
+                    String timeoutUrl =
+                        ((HttpServletRequest)httpServletRequest).getContextPath() + "/" + getTimeoutPage();
 
-        if (requestedSession != null && !currentWebSession.equalsIgnoreCase(requestedSession)) {
-//            logger.info("Inside session invalid");
-//            Long inMillis = System.currentTimeMillis();
-//            Date date = new Date(inMillis);
-//            logger.info("current time---------->"+date);
-//            String serverRelativeUrl = httpServletRequest.getContextPath();
-//            serverRelativeUrl = serverRelativeUrl.substring(0, serverRelativeUrl.lastIndexOf("/"));
-//            serverRelativeUrl = serverRelativeUrl + initParam;   
-//            logger.info("Server relative URL :: "+serverRelativeUrl);
-//            ((HttpServletResponse)response).sendRedirect(serverRelativeUrl);
-            logout();
-        } else {
-            chain.doFilter(request, response);
+                    logger.info("session is invalid! redirecting to timeoutpage : " + timeoutUrl);
+                    httpServletResponse.sendRedirect(timeoutUrl);
+                    return;
+                }
+            }
         }
+        filterChain.doFilter(request, response);
     }
-    
-    private void logout() {
 
-        FacesContext fctx = FacesContext.getCurrentInstance();
+    /** * * session shouldn't be checked for some pages. For example: for timeout page.. * Since we're redirecting to timeout page from this filter, * if we don't disable session control for it, filter will again redirect to it * and this will be result with an infinite loop... */
+    private boolean isSessionControlRequiredForThisResource(HttpServletRequest httpServletRequest) {
+        String requestPath = httpServletRequest.getRequestURI();
+        logger.info("Request URI - "+requestPath);
+        boolean controlRequired = !StringUtils.contains(requestPath, getTimeoutPage());
+        return controlRequired;
+    }
 
-        ExternalContext ectx = fctx.getExternalContext();
+    private boolean isSessionInvalid(HttpServletRequest httpServletRequest) {  
+        logger.info("httpServletRequest.getRequestedSessionId() -"+httpServletRequest.getRequestedSessionId());
+        logger.info("httpServletRequest.isRequestedSessionIdValid() -"+httpServletRequest.isRequestedSessionIdValid());
+        logger.info("httpServletRequest.getSession().getId() -"+httpServletRequest.getSession().getId());
+        logger.info("httpServletRequest.getSession().getId() equal to httpServletRequest.getRequestedSessionId() -"+httpServletRequest.getSession().getId().equals(httpServletRequest.getRequestedSessionId()));
+        logger.info("httpServletRequest.getUserPrincipal()"+httpServletRequest.getUserPrincipal());
+                
+        boolean sessionInValid =
+            (httpServletRequest.getRequestedSessionId() != null) && !httpServletRequest.isRequestedSessionIdValid();        
+        
+        // chk if anonymous role and current page is a protected one
+        if(!sessionInValid)
+        sessionInValid =
+                httpServletRequest.getUserPrincipal() == null && !(httpServletRequest.getRequestURI().contains("BrowseSearch") ||
+                                                                   httpServletRequest.getRequestURI().contains("MedDRAComponentsReport") ||
+                                                                   httpServletRequest.getRequestURI().contains("RiskDefinitionsSafetyTopicReport"));
+        
+        logger.info("is session invalid - "+ sessionInValid);
+        return sessionInValid;
+    }
 
-    //        accessLogger.info("LOGING OFF USER: " + ectx.getRemoteUser());
 
-        String url = ectx.getRequestContextPath() + "/adfAuthentication?logout=true&end_url=/faces/Home";
-        try {
-            ectx.redirect(url);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        fctx.responseComplete();
+    public void setTimeoutPage(String timeoutPage) {
+        this.timeoutPage = timeoutPage;
+    }
 
+    public String getTimeoutPage() {
+        return timeoutPage;
     }
 }
 
